d97319804ccec5d78d491f4ba0a1947136636451
vasivarga <vasi.vrg@gmail.com>
2024-07-30 20:30:33 +0300

    saptamana 4 - suite

--- intro_selenium/saptamana4/suita_teste.py
+++ intro_selenium/saptamana4/suita_teste.py
@@ -12,8 +12,10 @@ class TestSuite(unittest.TestCase):
     # numele metodei este predefinit si NU trebuie schimbat
     def test_suite(self):
 
+        # declaram o variabila TestSuite numit teste_de_rulat
+        # prin intermediul acestui obiect vom accesa metoda addTest din clasa TestSuite
+        # metoda addTest primeste ca si parametru o clasa de teste procesata de metoda loadTestsFromTestCase
         teste_de_rulat = unittest.TestSuite()
-
         teste_de_rulat.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestKeys))
         teste_de_rulat.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestAlerts))
         teste_de_rulat.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestLogin))
@@ -25,6 +27,8 @@ class TestSuite(unittest.TestCase):
         #     unittest.defaultTestLoader.loadTestsFromTestCase(TestLogin)
         # )
 
+        # vom crea o variabila de tip HTMLTestRunner care ne ajuta in executarea testelor din suita
+        # si ne va genera un raport HTML cu rezultatele testelor
         runner = HtmlTestRunner.HTMLTestRunner(
             combine_reports=True,
             report_title="Titlu raport executie",
